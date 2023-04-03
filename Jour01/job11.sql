d887064074c0e7fb52d760980c61783be0775f71
mathieu.pinaud@laplateforme.io <mathieu.pinaud@laplateforme.io>
2023-04-03 17:55:11 +0200

    journée finie

---
+++ Jour01/job11.sql
@@ -0,0 +1,1 @@
+SELECT * FROM etudiants WHERE prenom = 'Gertrude' AND nom = 'Dupuis';
---
+++ Jour01/job12.sql
@@ -0,0 +1,2 @@
+INSERT INTO etudiants (nom, prenom, age, email) VALUES ('Martin', 'Dupuis', 18, 'martin.dupuis@laplateforme.io');
+SELECT * FROM etudiants WHERE nom = 'Dupuis';
